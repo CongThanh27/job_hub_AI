98b93be2c670f951fe2b2c8aa030960eb050941b
CongThanh27 <nguyencongthanh280702@gmail.com>
2024-03-11 15:32:06 +0700

    change

--- __init__.py
+++ __init__.py
@@ -5,20 +5,3 @@ from collaborative_filtering import CF
 from handle_data import process_data, encode_data
 from recommendation import user_user_collaborative_filtering, item_item_collaborative_filtering, get_recommendation_user_by_job, get_recommendation_job_by_user
 from db_connection import connect_to_database_sql, connect_to_mongodb
-
-
-def __getattr__(name: str) -> t.Any:
-    if name == "__version__":
-        import importlib.metadata
-        import warnings
-
-        warnings.warn(
-            "The '__version__' attribute is deprecated and will be removed in"
-            " Flask 3.1. Use feature detection or"
-            " 'importlib.metadata.version(\"flask\")' instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return importlib.metadata.version("flask")
-
-    raise AttributeError(name)
